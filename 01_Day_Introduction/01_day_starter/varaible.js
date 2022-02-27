256e288f9404ae226a0b4ec5507ccf54b7ef7050
Gramm OBE <85841088+olusiekwin@users.noreply.github.com>
2022-02-27 21:11:30 +0300

    Add files via upload

---
+++ 01_Day_Introduction/01_day_starter/varaible.js
@@ -0,0 +1,20 @@
+// Declaring different variables of different data types
+
+let firstName = "Graham"; // first name of a person
+let lastName = "Olusiekwin"; // last name of a person
+let country = "Kenya"; // country
+let city = "Nairobi "; // capital city
+let age = 100; // age in years
+let isMarried = true;
+
+// Declaring variables with number values
+
+const gravity = 9.81; // earth gravity  in m/s2
+const boilingPoint = 100; // water boiling point, temperature in oC
+const PI = 3.14; // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+
+let name = "gramm", //name of a person
+  job = "Developer",
+  live = "Kenya";
