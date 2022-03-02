e9e776c8a05860f792b032c7996a6a09d24198c3
Gramm OBE <85841088+olusiekwin@users.noreply.github.com>
2022-03-02 19:47:25 +0300

    Add files via upload

---
+++ 03_Day_Booleans_operators_date/Arithmetic_Operators.js
@@ -0,0 +1,9 @@
+let numOne = 4;
+let numTwo = 3;
+let sum = numOne + numTwo; //7
+let diff = numOne - numTwo; //1
+let mult = numOne * numTwo; //12
+let div = numOne / numTwo; //1.33333333
+let remainder = numOne % numTwo; //1
+let powerOf = numOne ** numTwo; //64
+console.log(sum, diff, mult, div, remainder, powerOf); //7,1,12,1.33,1,64
---
+++ 03_Day_Booleans_operators_date/Assignment/boolean_values.js
@@ -0,0 +1,6 @@
+let isLight0n = true;
+let isRaining = false;
+let isHungry = false;
+let isMarried = false;
+let truValue = 4>3;   // true
+let  falseValue = 4<3;  // false
