26680706d4a31906981c5f352c1a5627f1175ef5
Gramm OBE <85841088+olusiekwin@users.noreply.github.com>
2022-03-15 16:09:36 +0300

    Add files via upload

---
+++ Day_4_Conditionals/Conditionals/If.js
@@ -0,0 +1,9 @@
+let num = 3;
+if (num > 0) {
+  console.log(`${num} is a positive number`); //3 is a positive number
+}
+
+let isRaining = true;
+if (isRaining) {
+  console.log("Remember to stay warm with your raincoat"); //Remember to stay warm with your raincoat
+}
---
+++ Day_4_Conditionals/Conditionals/If_Else.js
@@ -0,0 +1,32 @@
+let num = 3;
+if (num > 0) {
+  console.log(`${num} is a positive number`);
+} else {
+  console.log(`${num} is a negative number`); // 3 is a positive number
+}
+num = -4;
+if (num < 0) {
+  console.log(`${num} is a positive number`);
+} else {
+  console.log(`${num} is a negative number`);
+}
+
+/**
+ * Running] node "c:\Users\admin\JS\30_Days_With_JS\Day_4_Conditionals\Conditionals\If_Else.js"
+ is a positive number
+4 is a positive number */
+
+let isRaining = true;
+if (isRaining) {
+    console.log("You need a rain coat.");
+} else{
+    console.log("No need for heavy cloathing today");
+}   
+
+/*// not raining
+let isNotRaining = false;
+f (isRaining) {
+   console.log("You need a rain coat.");
+ else{
+   console.log("No need for heavy cloathing today");
+}*/
